7e800e018077b2d758a947b4d5d4024e48ceee54
MiniDigger <admin@minidigger.me>
2019-10-21 19:52:42 +0200

    Initial Implementation, 100% untested

--- README.md
+++ README.md
@@ -13,7 +13,21 @@ Simple plugin for the irc client [thelounge](https://thelounge.chat) that allows
 
 # Usage
 
-TODO
+`/giphy search <term>` -> Search a gif  
+`/giphy random` -> Get a random gif
+
+# Setup
+
+You will need an API key to interact with giphy. You can create an app and obtain an api key [here](https://developers.giphy.com/dashboard/?create=true)
+(select I only want to use the GIPHY API, the name and description don't matter).
+
+Once you got the key, enter it via `/giphy key <your-api-key>`.  
+Then you are all set.
+
+# Content Rating
+
+You can disallow gifs with certain content ratings using the `/giphy rating` command.
+See [this page](https://developers.giphy.com/docs/optional-settings#rating) for more info
 
 # Development
 
@@ -46,3 +60,6 @@ ln index.js ../thelounge-home/packages/node_modules/thelounge-plugin-giphy/index
 You can then just edit and commit the files in the project dir and restart thelounge
  on every change you do and the changes will be picked up.
  
+# Attribution
+
+![https://giphy.com](img/giphy.png)
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 "use strict";
 
+const axios = require('axios');
+const qs = require('qs');
 const Msg = require("thelounge/src/models/msg");
+const log = require("thelounge/src/log");
+const Helper = require("thelounge/src/helper");
+const Utils = require("thelounge/src/command-line/utils");
+const fs = require("fs");
+const path = require('path');
 
-const code = "";
+Helper.setHome(process.env.THELOUNGE_HOME || Utils.defaultHome()); // TODO shouldn't be necessary...
+const configFile = path.resolve(Helper.getPackagesPath(), "giphy.json");
 
+let config = {
+    apiKey: "your-api-key",
+    rating: "g",
+};
 let thelounge = null;
+
+const ratings = ['G', 'PG', 'PG-13', 'R'];
 
 function sendMessage(text, chan, client) {
     chan.pushMessage(client.client, new Msg({
@@ -20,17 +34,111 @@ function sendErrorMessage(text, chan, client) {
     }));
 }
 
+function saveConfig() {
+    fs.writeFile(configFile, JSON.stringify(config), "utf-8", (err) => {
+        if (err) log.error(err);
+        log.info("[Giphy] Successfully wrote config to file " + configFile);
+    });
+}
+
+function loadConfig() {
+    fs.readFile(configFile, "utf-8", function (err, data) {
+        if (err) log.error(err);
+        try {
+            config = JSON.parse(data);
+            log.info("[Giphy] Loaded config from " + configFile);
+        } catch (error) {
+            log.error("[Giphy] Error while loading config: " + error);
+            saveConfig();
+        }
+    });
+}
+
+function getGif(term) {
+    return new Promise((resolve, reject) => {
+        const args = qs.stringify({
+            q: term,
+            api_key: config.apiKey,
+            limit: 1,
+            rating: config.rating,
+        });
+        axios.get('http://api.giphy.com/v1/gifs/search?' + args)
+            .then(function (response) {
+                if (response && response.data && response.data.data && response.data.data.embed_url) {
+                    resolve(response.data.data.embed_url);
+                } else {
+                    reject("Error while getting gif: " + response);
+                }
+            })
+            .catch(function (error) {
+                reject("Error while getting gif: " + error);
+            });
+    });
+}
+
+function getRandomGif() {
+    return new Promise((resolve, reject) => {
+        const args = qs.stringify({
+            api_key: config.apiKey,
+            rating: config.rating,
+        });
+        axios.get('http://api.giphy.com/v1/gifs/random?' + args)
+            .then(function (response) {
+                if (response && response.data && response.data.data && response.data.data.embed_url) {
+                    resolve(response.data.data.embed_url);
+                } else {
+                    reject("Error while getting gif: " + response);
+                }
+            })
+            .catch(function (error) {
+                reject("Error while getting gif: " + error);
+            });
+    });
+}
+
 const giphyCommand = {
     input: function (client, target, command, args) {
         if (args.length === 0) {
-            sendErrorMessage("Usage: /giphy <stuff>", target.chan, client);
+            sendErrorMessage("Usage: /giphy <random|search|rating|key>", target.chan, client);
             return;
         }
         switch (args[0]) {
-            case "stuff":
-
+            case "key":
+                if (args.length === 1) {
+                    sendErrorMessage("Usage: /giphy key <your-api-key>", target.chan, client);
+                    sendMessage("For more info see <someurl>", target.chan, client);
+                } else {
+                    config.apiKey = args[1];
+                    saveConfig();
+                    sendMessage("API Key set. Happy giphying!", target.chan, client);
+                }
+                break;
+            case "rating":
+                if (args.length === 1 || !ratings.includes(args[1])) {
+                    sendErrorMessage("Usage: /giphy rating <" + ratings.join("|") + ">", target.chan, client);
+                    sendMessage("For more info see https://developers.giphy.com/docs/optional-settings#rating", target.chan, client);
+                } else {
+                    config.rating = args[1];
+                    saveConfig();
+                    sendMessage("Rating set to " + config.rating, target.chan, client);
+                }
+                break;
+            case "search":
+                if (args.length === 1) {
+                    sendErrorMessage("Usage: /giphy search <term>", target.chan, client);
+                } else {
+                    getGif(args.splice(1).join(" "))
+                        .then(gif => client.runAsUser(gif, target.chan.id))
+                        .catch(error => sendErrorMessage(error, target.chan, client));
+                }
+                break;
+            case "random":
+                getRandomGif()
+                    .then(gif => client.runAsUser(gif, target.chan.id))
+                    .catch(error => sendErrorMessage(error, target.chan, client));
+                break;
             default:
-                sendErrorMessage("Usage: /giphy <stuff>", target.chan, client);
+                sendErrorMessage("Usage: /giphy <random|search|rating|key>", target.chan, client);
                 break;
         }
     },
@@ -41,5 +149,6 @@ module.exports = {
     onServerStart: api => {
         thelounge = api;
         thelounge.Commands.add("giphy", giphyCommand);
+        loadConfig();
     },
 };
